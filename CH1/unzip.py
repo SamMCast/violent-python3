7e3c724bb251be3603e3ac3788abdbc02cfd736b
SamMCast <namelast49@gmail.com>
2022-03-05 18:32:41 -0500

    added multithreading support. Removed the verbose functionality

--- CH1/unzip.py
+++ CH1/unzip.py
@@ -1,36 +1,25 @@
 import zipfile
 import argparse
 import os
+from threading import Thread, Lock
 
-def extractFile(zFile: zipfile.ZipFile, dictFile: str, verbose: bool = False):
-    with open(dictFile, 'r') as dictionaryFile:
-        trial = 0
-        for line in dictionaryFile.readlines():
-            password = line.strip("\n")
-            try:
-                if verbose:
-                    print("Trial: {0} Attempting zip file crack with password {1}".format(trial +1, password))
-                zFile.extractall(pwd=password.encode())
-                print("Success. Password =", password)
-                exit(0)
-            except RuntimeError:
-                 pass
-            trial = trial +1
-        if verbose:
-            print("Unable to determine password upon performing {0} different passwords.".format(trial+1))
-        else:
-            print("Unable to find password after exhaustive search. ")
+def extractFile(zFile: zipfile.ZipFile, password: str):
+    try:
+        zFile.extractall(pwd=password.encode())
+        print("Success. Password =", password)
+        exit(0)
+    except RuntimeError:
+            pass
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("zipFile", help="Password encrypted zipfile")
     parser.add_argument("dictionary", help="A wordlist file")
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args()
 
     zipFile = args.zipFile
     dictFile = args.dictionary
-    verbose = args.verbose
 
     if not (os.path.isfile(zipFile) and zipfile.is_zipfile(zipFile.encode())):
         print("Error. First argument {0} is either not a regular zip file.".format(zipFile))
@@ -41,7 +30,13 @@ def main():
     
 
     zFile = zipfile.ZipFile(zipFile)
-    extractFile(zFile, dictFile, verbose)
+    lock = Lock()
+    with open(dictFile, 'r') as dictFile:
+        for line in dictFile.readlines():
+            password = line.strip('\n')
+            t = Thread(target=extractFile, kwargs={"zFile": zFile, "password": password})
+            t.start()
+    
 
 
 if __name__ == "__main__":
